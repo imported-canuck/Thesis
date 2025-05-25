8fecbdaa03dcb0fea1b276c457cca84b5665062a
C. A. <146338867+imported-canuck@users.noreply.github.com>
2025-05-24 21:50:27 -0400

    Version control

--- external_isospectralization/code_for_3D/test.py
+++ external_isospectralization/code_for_3D/test.py
@@ -7,12 +7,12 @@ params = OptimizationParams()
 params.min_eval_loss = 0.0001
 params.evals = [20]
 params.numsteps = 3000
-params.volume_reg = 1e3 # Default is 1e1, but this causes protrusion to be inwards rather than outwards.
+params.volume_reg = 1e3 ## Modified
 
 [VERT, TRIV] = load_mesh('data/round_cuber_1000/');
 mesh = prepare_mesh(VERT,TRIV,'float32')
 
-[VERT_t, TRIV_t] = load_mesh('data/round_cuber_out_1000/')
+[VERT_t, TRIV_t] = load_mesh('data/bunny/')
 evals_t = calc_evals(VERT_t,TRIV_t)
 
-run_optimization(mesh = mesh, target_evals = evals_t, out_path = 'results/test_cube_out_disp', params = params)
+run_optimization(mesh = mesh, target_evals = evals_t, out_path = 'results/bunny_disp', params = params)
